c76ab74aa85e74d14f45b12acaccc4470133670f
Evan Frank <evan.frank@marketcypher.com>
2024-11-28 13:37:05 -0500

    adding controls and events around data pulling and logging, added meta data controls as well

--- SurfingProject/surfing_project/surfing_project/assets.py
+++ SurfingProject/surfing_project/surfing_project/assets.py
@@ -27,7 +27,10 @@ def real_time_swell_list(localDB: postgres_con) -> MaterializeResult:
     df = df.dropna(axis=1)
 
     engine = localDB.make_con()
-    df.to_sql("NewFiles", con=engine, if_exists='replace', index='False')
+    df.to_sql("NewFiles",
+              con=engine,
+              if_exists='replace',
+              index=False)
 
     # Print the DataFrame to the Metadata
     return MaterializeResult(
@@ -38,12 +41,89 @@ def real_time_swell_list(localDB: postgres_con) -> MaterializeResult:
     )
 
 
-@asset(deps=[real_time_swell_list])
-def bouy_data(localDB: postgres_con) -> MaterializeResult:
+@asset
+def bouy_names(localDB: postgres_con) -> MaterializeResult:
+    engine = localDB.make_con()
+
+    bouy_meta = pd.read_xml("https://www.ndbc.noaa.gov/activestations.xml")
+
+    sql = "SELECT * FROM public.\"BouyMeta\";"
+    bouy_loaded = pd.read_sql(sql, con=engine)
+
+    existing_bouys = bouy_loaded['id'].values
+    bouy_meta = bouy_meta[~bouy_meta['id'].isin(existing_bouys)]
+
+    bouy_meta.to_sql("BouyMeta",
+                     con=engine,
+                     if_exists='append',
+                     index=False)
+
+    return MaterializeResult(
+        metadata={
+            "num_records": len(bouy_meta),
+            "preview": MetadataValue.md(bouy_meta.head().to_markdown()),
+        }
+    )
+
+
+@asset(deps=[bouy_names])
+def east_coast_bouy_names(localDB: postgres_con) -> MaterializeResult:
+    engine = localDB.make_con()
+
+    sql = "SELECT * FROM public.\"BouyMeta\";"
+
+    bouy_meta = pd.read_sql(sql,
+                            con=engine)
+    bouy_meta = bouy_meta[bouy_meta["lon"].between(-84, -55)
+                          & bouy_meta["lat"].between(23, 47)]
+
+    bouy_meta.to_sql("BouyMetaNorthEast",
+                     con=engine,
+                     if_exists='replace',
+                     index=False)
+
+    # marking metadata
+    m = folium.Map(location=[bouy_meta['lat'].iloc[0],
+                             bouy_meta['lon'].iloc[0]], zoom_start=5)
+
+    for index, row in bouy_meta.iterrows():
+        folium.Marker(
+            location=[row['lat'], row['lon']],
+            popup=row['name']
+        ).add_to(m)
+
+    buffer = BytesIO()
+    img = m._to_png(5)
+    img = Image.open(io.BytesIO(img))
+    img.save(buffer, format="PNG")
+    image_data = base64.b64encode(buffer.getvalue())
+
+    # Convert the image to Markdown to preview it within Dagster
+    md_content = f"![img](data:image/png;base64,{image_data.decode()})"
+
+    return MaterializeResult(
+        metadata={
+            "num_records": len(bouy_meta),
+            "preview": MetadataValue.md(bouy_meta.head().to_markdown()),
+            "map": MetadataValue.md(md_content)
+        }
+    )
+
+
+@asset(deps=[real_time_swell_list, east_coast_bouy_names])
+def real_time_bouy_data(localDB: postgres_con) -> MaterializeResult:
     engine = localDB.make_con()
 
     sql = "SELECT * FROM public.\"NewFiles\";"
     files = pd.read_sql(sql, con=engine)
+
+    sql = "SELECT * FROM public.\"BouyMetaNorthEast\";"
+
+    bouy_meta = pd.read_sql(sql, con=engine)
+
+    bouy_list = bouy_meta['id'].values
+    files = files[files['ID'].isin(bouy_list)]
+
     wave_files = files[files["Type"] == "spec"]
     wave_files_i = wave_files["Name"].to_list()
 
@@ -64,7 +144,7 @@ def bouy_data(localDB: postgres_con) -> MaterializeResult:
 
         wave_data = pd.concat([data, wave_data])
 
-    wave_data.to_sql("BouyData",
+    wave_data.to_sql("BouyDataCurrent",
                      con=engine,
                      if_exists='replace',
                      index='False')
@@ -78,42 +158,17 @@ def bouy_data(localDB: postgres_con) -> MaterializeResult:
     )
 
 
-@asset
-def bouy_names(localDB: postgres_con) -> MaterializeResult:
-    # engine = localDB.make_con()
+"""
+@asset(deps=[real_time_bouy_data])
+def store_new_data(localDB: postgres_con) -> MaterializeResult:
+    engine = localDB.make_con()
+    sql = "SELECT * FROM public.\"BouyDataCurrent\";"
+    bouy_current = pd.read_sql(sql, con=engine)
 
-    bouy_meta = pd.read_xml("https://www.ndbc.noaa.gov/activestations.xml")
+    sql = "SELECT * FROM public.\"BouyData\";"
+    bouy_hist = pd.read_sql(sql, con=engine)
+    bouy_hist
 
-    bouy_meta = bouy_meta[bouy_meta["lon"].between(-84, -55)
-                          & bouy_meta["lat"].between(23, 47)]
-
-    # bouy_meta.to_sql("BouyMeta",
-    #                 con=engine,
-    #                 if_exists='replace',
-    #                 index='False')
-
-    m = folium.Map(location=[bouy_meta['lat'].iloc[0],
-                             bouy_meta['lon'].iloc[0]], zoom_start=5)
-
-    # Add markers for each location
-    for index, row in bouy_meta.iterrows():
-        folium.Marker(
-            location=[row['lat'], row['lon']],
-            popup=row['name']
-        ).add_to(m)
-
-    buffer = BytesIO()
-    img = m._to_png(5)
-    img = Image.open(io.BytesIO(img))
-    img.save(buffer, format="PNG")
-    image_data = base64.b64encode(buffer.getvalue())
-
-    # Convert the image to Markdown to preview it within Dagster
-    md_content = f"![img](data:image/png;base64,{image_data.decode()})"
-    return MaterializeResult(
-        metadata={
-            "num_records": len(bouy_meta),
-            "preview": MetadataValue.md(bouy_meta.head().to_markdown()),
-            "map": MetadataValue.md(md_content)
-        }
-    )
+    bouy_hist_ids = bouy_hist['id'].values
+    bouy_hist_stamp = bouy_hist[]
+    """
